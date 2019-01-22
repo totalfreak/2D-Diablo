9fc1be6de5e1d2a3d2a1427041a33a47b0653f72
Totalfreak <dgkartin@live.dk>
2019-01-22 21:59:25 +0100

    Begun importing new shitty sprites, and making enemy attack system

--- Scenes/Enemies/Enemy.gd
+++ Scenes/Enemies/Enemy.gd
@@ -2,44 +2,79 @@ extends KinematicBody2D
 
 export var moveSpeed = 100
 
-onready var leftRay = get_node("RayCast2DLeft")
-onready var rightRay = get_node("RayCast2DRight")
-
+onready var leftSurfaceRay = get_node("surfaceRayLeft")
+onready var rightSurfaceRay = get_node("surfaceRayRight")
+onready var leftPlayerRay = get_node("playerRayLeft")
+onready var rightPlayerRay = get_node("playerRayRight")
+onready var player = get_tree().get_nodes_in_group("Player")[0]
+onready var attackTimer = get_node("AttackTimer")
 var rayBools = [false, false]
+var playerBools = [false, false]
 
 var movingRight = false
+var attacking = false
 
 var motion = Vector2()
 
 func _ready():
-	leftRay.add_exception(get_tree().get_nodes_in_group("Player")[0])
-	rightRay.add_exception(get_tree().get_nodes_in_group("Player")[0])
+	#Add exceptions to rays
+	leftSurfaceRay.add_exception(get_tree().get_nodes_in_group("Player")[0])
+	rightSurfaceRay.add_exception(get_tree().get_nodes_in_group("Player")[0])
+	leftPlayerRay.add_exception(get_tree().get_nodes_in_group("Solids")[0])
+	rightPlayerRay.add_exception(get_tree().get_nodes_in_group("Solids")[0])
 	pass
 
 func _physics_process(delta):
-	rayBools[0] = leftRay.is_colliding()
-	rayBools[1] = rightRay.is_colliding()
-		
-	if rayBools[0] or rayBools[1]:
-		if not movingRight and rayBools[0]:
-			motion.x = -moveSpeed
-		elif movingRight and rayBools[1]:
-			motion.x = moveSpeed
-		#Nothing left but something right, flip move direction
-		if not rayBools[0] and rayBools[1]:
-			movingRight = true
-			#Nothing right but something left, flip move direction
-		elif rayBools[0] and not rayBools[1]:
-			movingRight = false
-		
-		motion = move_and_slide(motion)
-		
-		if motion.x == 0:
+	#Save status of rays
+	rayBools[0] = leftSurfaceRay.is_colliding()
+	rayBools[1] = rightSurfaceRay.is_colliding()
+	playerBools[0] = leftPlayerRay.is_colliding()
+	playerBools[1] = rightPlayerRay.is_colliding()
+	
+	if not attacking:
+		if rayBools[0] or rayBools[1]:
+			#Move left
+			if not movingRight and rayBools[0]:
+				rightPlayerRay.enabled = false
+				leftPlayerRay.enabled = true
+				motion.x = -moveSpeed
+				#If player is in attack range
+				if playerBools[0] and leftPlayerRay.get_collider().get_groups()[0] == "Player":
+					print(leftPlayerRay.get_collider().get_groups())
+					attacking = true
+					player._Get_Attacked(10, "left")
+					attackTimer.start()
+			#Move right
+			elif movingRight and rayBools[1]:
+				rightPlayerRay.enabled = true
+				leftPlayerRay.enabled = false
+				motion.x = moveSpeed
+				#If player is in attack range
+				if playerBools[1] and rightPlayerRay.get_collider().get_groups()[0] == "Player":
+					print(rightPlayerRay.get_collider().get_groups())
+					attacking = true
+					player._Get_Attacked(10, "right")
+					attackTimer.start()
 			#Nothing left but something right, flip move direction
-			if not movingRight and rayBools[1]:
+			if not rayBools[0] and rayBools[1]:
 				movingRight = true
-			#Nothing right but something left, flip move direction
-			elif movingRight and rayBools[0]:
+				#Nothing right but something left, flip move direction
+			elif rayBools[0] and not rayBools[1]:
 				movingRight = false
-	else:
-		pass
+			
+			motion = move_and_slide(motion)
+			
+			if motion.x == 0:
+				#Nothing left but something right, flip move direction
+				if not movingRight and rayBools[1]:
+					movingRight = true
+				#Nothing right but something left, flip move direction
+				elif movingRight and rayBools[0]:
+					movingRight = false
+		else:
+			pass
+
+
+func _on_AttackTimer_timeout():
+	attacking = false
+	attackTimer.stop()
--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -2,6 +2,8 @@ extends KinematicBody2D
 
 var motion = Vector2()
 
+export var health = 100
+var dead = false
 const GRAVITY = 20
 const UP_VECTOR = Vector2(0, -1)
 export var moveSpeed = 300
@@ -10,15 +12,20 @@ export var dampSpeed = 20
 
 var hasWon = false
 
+onready var attackTimer = get_node("AttackTimer")
+onready var takeDamageTimer = get_node("TakeDamageTimer")
+var takingDamage = false
+var knockbacked = false
+export var knockbackDelay = 0.5
+
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
+	takeDamageTimer.connect("timeout", self, "_Stop_Taking_Damage")
 	pass
 
 func _physics_process(delta):
 	#Subtracting gravity
 	motion.y += GRAVITY
-	if not hasWon:
+	if not hasWon and not dead and not takingDamage:
 		if Input.is_action_pressed("ui_right"):
 			$Sprite.flip_h = false
 			# If on floor, play run anim else do either fall or jump anim
@@ -44,7 +51,7 @@ func _physics_process(delta):
 		elif not is_on_floor():
 			#Slow down in air if no direction is pressed
 			if not Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
-				motion.x = lerp(motion.x, 0, dampSpeed/10 * delta)
+				motion.x = lerp(motion.x, 0, dampSpeed/5 * delta)
 			#Play jump and fall animations
 			if motion.y < 0:
 				$Sprite.play("Jump")
@@ -53,13 +60,34 @@ func _physics_process(delta):
 		else:
 			motion.x = lerp(motion.x, 0, dampSpeed * delta)
 			$Sprite.play("Idle")
-	else:
+	elif hasWon:
 		motion.x = 0
 		$Sprite.play("Won")
 	
 	#Jumping when on floor
-	if is_on_floor() and Input.is_action_just_pressed("ui_accept"):
+	if not dead and not hasWon and not takingDamage and is_on_floor() and Input.is_action_just_pressed("ui_accept"):
 		motion.y = -jumpSpeed
 	#Setting motion vector and moving
 	motion = move_and_slide(motion, UP_VECTOR)
+	pass
+
+
+func _Get_Attacked(var damage, var direction):
+	move_and_slide(Vector2(0,0), UP_VECTOR)
+	takingDamage = true
+	takeDamageTimer.stop()
+	takeDamageTimer.start()
+	$Sprite.play("Get_Hit")
+	health-=damage
+	
+	if health <= 0 and not dead:
+		_Die()
+	print(str(damage) + " " + direction)
+
+func _Stop_Taking_Damage():
+	takingDamage = false
+	takeDamageTimer.stop()
+
+func _Die():
+	dead = true
 	pass
